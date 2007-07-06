d2aa7a0ec16604be95f7d5f3182e5f9f4a9b762c
dominic.marcotte <dominic.marcotte@32b8cc01-7633-0410-beea-25b907642fa6>
2007-07-06 17:03:02 +0000

    Création des UniteMesure

---
+++ trunk/ALaSoupe/src/dominicm/alasoupe/recettes/ingredients/UniteMesureImpl.java
@@ -0,0 +1,16 @@
+package dominicm.alasoupe.recettes.ingredients;
+
+public class UniteMesureImpl extends UniteMesure
+{
+	private String uniteMesure;
+
+	public String getUniteMesure()
+	{
+		return uniteMesure;
+	}
+
+	public void setUniteMesure(String uniteMesure)
+	{
+		this.uniteMesure = uniteMesure;
+	}
+}

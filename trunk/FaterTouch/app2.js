08cb92698d9610b1e8bda9dc4003823bdf48345b
dominic.marcotte@gmail.com <dominic.marcotte@gmail.com@32b8cc01-7633-0410-beea-25b907642fa6>
2012-02-09 04:51:24 +0000

    Le volet des réponses est fonctionnel La bonne ou mauvaise réponse est comptabilisée

--- trunk/FaterTouch/app2.js
+++ trunk/FaterTouch/app2.js
@@ -1,347 +1,304 @@
 /*
  * TypeError: Cannot read property 'length' of undefined
- * 
+ *
  * Veut souvent dire : ne trouve pas le fichier de données
  */
-var questions;
 
-       Ext.regModel('Question', {
-            fields: ['textQuestion', 'reponses', 'bonneReponse', 'texteExplicatif']    
-        });
-        
-        var store = new Ext.data.Store({
-            model: 'Question',
-            proxy: {
-                type: 'ajax',
-                url: 'data.xml',
-                reader: {
-                    type: 'xml',
-                    record: 'question'
-                }
-            },
-            listeners: {
-                single: true,
-                datachanged: function(){
-                    Ext.getBody().unmask();
-                    questions = [];
-                    
-                    store.each(function(rec){
-                        //alert("Lecture " + rec.get('textQuestion'));
-                        
-                        alert("rec.get('reponses')=" + rec.get('reponses'));
-                        /*rec.get('reponses').each(function(rec2){
-                            alert("rec2 = " + rec2.get('reponse'));
-                        };*/
-                        
-                        questions.push(insertQuestion(
-                                                "Question x : " + rec.get('textQuestion'),
-                                                [
-                                                    "Auprès des personnes handicapées", 
-                                                    "Auprès des groupes religieux", 
-                                                    "Auprès des immigrants"
-                                                ],
-                                                rec.get('bonneReponse'),
-                                                rec.get('textExplication'),
-                                                1
-                                            )
-                        );
-                    });
-                    
-                    
-                    var questionsPanel = new Ext.Panel({
-                        fullscreen: true,
-                        layout: 'card',
-                        items: questions
-                    });                 
-                    
-                    /*var carousel = new Ext.Carousel({
-                        items: items,
-                        itemId: 'carousel'
-                    });*/
-                    panel.add(questionsPanel);
-                    panel.doLayout();
-                }
-            }    
-        });
-        store.read();
-
+var questions = [];
 var question2;
-
 var overlay;
+var rootPanel;
+var resultatFinal = 0;
 
-
-var popup = new Ext.Panel({
-                    floating: true,
-                    modal: true,
-                    centered: true,
-                    width: 200,
-                    styleHtmlContent: true,
-                    html: '<p>This is a modal, centered and floating panel. hideOnMaskTap is true by default so ' +
-                          'we can tap anywhere outside the overlay to hide it.</p>',
-                    dockedItems: [{
-                        dock: 'top',
-                        xtype: 'toolbar',
-                        title: 'Overlay Title'
-                    }],
-                    scroll: 'vertical'
-                });
-
-      
-
-function creationBouton(texte, succes, reponse, nextActiveItem)
+var resultat = new Ext.Panel(
 {
-return new Ext.Button({
-                        ui  : 'confirm',
-                        text: texte,
-                        margin: 10,
-                        width: "400px",
-                        handler: function() {
-                                var bravo = (succes) ? 'Bravo, vous avez raison!\n\n' : 
-                                    'Ce n\'est pas tout à fait ça.';
+	floating : true,
+	centered : true,
+	modal : true,
+	width : '400px',
 
-                                var msg = new Ext.MessageBox({
-                                    html : 'text',
-                                    width: '500px'
-                                });
-                                msg.alert('Réponse', "<p style=\"margin: 10px; font-size: 1.2em; font-weight: bold;\">" + bravo + "</p>" + reponse, function() {
-                                    rootPanel.setActiveItem(nextActiveItem, 'cube');
-                                });
-                        }
-                    });
+	dockedItems : [
+	{
+		dock : 'top',
+		xtype : 'toolbar',
+		title : 'Résultat'
+	}],
+	items : [
+	{
+		html : "<h1>100 %</h1>recommencer."
+	}]
+});
+
+// ----------------------------------
+// --- chargement du modèle : questions
+
+Ext.regModel('Question',
+{
+	fields : ['textQuestion', 'bonneReponse', 'texteExplicatif'],
+	hasMany :
+	{
+		model : 'Reponse',
+		name : 'reponses'
+	},
+	proxy :
+	{
+		type : 'ajax',
+		url : 'data.xml',
+		reader :
+		{
+			type : 'xml',
+			record : 'question'
+		}
+	}
+});
+
+Ext.regModel('Reponse',
+{
+	fields : ['reponse'],
+	proxy :
+	{
+		type : 'ajax',
+		url : 'data.xml',
+		reader :
+		{
+			type : 'xml',
+			record : 'reponse'
+		}
+	}
+});
+
+var store = new Ext.data.Store(
+{
+	model : 'Question',
+	autoLoad : true
+});
+
+store.load(
+{
+	callback : function()
+	{
+
+		var itemCpt = 0;
+
+		store.each(function(question)
+		{
+			console.log("log Question : " + question.get('textQuestion') + ":");
+
+			var choixReponses = [];
+			question.reponses().each(function(reponse)
+			{
+				console.log("	Reponses : " + reponse.raw.textContent);
+				choixReponses.push(reponse.raw.textContent);
+			});
+			
+			questions.push(insertQuestion("<div class='titreNumQuestion'>Question " + (itemCpt + 1) + "</div>" + question.get('textQuestion'), choixReponses, question.get('bonneReponse'), question.get('texteExplicatif'), ++itemCpt));
+
+		});
+	}
+});
+
+// ----------------------------------
+
+function insertQuestion(question, arrayReponses, bonneReponse, texteExplication, nextActiveItem)
+{
+
+	//var titreQuestion =
+	//var items = [creationBouton("texte", "1", null,
+	// 1)].concat(creationBoutons(arrayReponses, bonneReponse, texteExplication,
+	// nextActiveItem));
+	var items = [new Ext.Panel(
+	{
+		id : 'question',
+		html : '<div class="question">' + question + '</div>'
+
+	})].concat(creationBoutons(arrayReponses, bonneReponse, texteExplication, nextActiveItem));
+
+	return new Ext.Panel(
+	{
+		//floating : true,
+		centered : true,
+		//modal : true,
+		//width: '400px',
+
+		layout : Ext.is.Phone ? 'fit' :
+		{
+			type : 'vbox',
+			align : 'center',
+			pack : 'center'
+		},
+
+		dockedItems : [
+		{
+			dock : 'top',
+			xtype : 'toolbar',
+			title : 'Questionnaire Fater'
+		}],
+
+		items : items
+
+	});
 }
 
 function creationBoutons(arrayReponses, bonneReponse, texteExplication, nextActiveItem)
 {
-    var boutons = new Array();
-    
-    Ext.each(arrayReponses, function(reponse, index) 
-    {
-        boutons.push(creationBouton(reponse, true, texteExplication, nextActiveItem));
-    });
-    return boutons;
+	var boutons = new Array();
+
+	var noReponse = 1;
+	Ext.each(arrayReponses, function(reponse, index)
+	{
+		var estBonneReponse = (noReponse == bonneReponse) ? true : false;
+
+		boutons.push(creationBouton(reponse, estBonneReponse, texteExplication, nextActiveItem));
+		noReponse++;
+	});
+	return boutons;
 }
 
-function insertQuestion(question, arrayReponses, bonneReponse, texteExplication, nextActiveItem)
+function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem)
 {
-    return new Ext.Panel({
-        floating: true,
-        centered: true,
-        modal: true,
-        /*width: '400px',*/
-        
-        layout: Ext.is.Phone ? 'fit' : {
-                type: 'vbox',
-                align: 'center',
-                pack: 'center'
-                },
-        
-        dockedItems: [{
-            dock: 'top',
-            xtype: 'toolbar',
-            title: question
-            }],
-            
-    	items: creationBoutons(arrayReponses, bonneReponse, texteExplication, nextActiveItem)
-            
-    });
-}
 
-var rootPanel;
+	/*
+	 var datePicker = new Ext.DatePicker({
+	 useTitles: true,
+	 value: {
+	 day: 23,
+	 month: 2,
+	 year: 1984
+	 }
+	 });*/
+
+	var reponse = "<div class='bravoErreur'>" + ((estBonneReponse) ? 'Bravo, vous avez raison!' : 'Ce n\'est pas tout à fait ça.') + "</div>" + texteExplication;
 
 
+	// volet de la bonne réponse
+	var sheet = new Ext.Sheet(
+	{
+		modal : true,
+		centered : false,
+		//hideOnMaskTap : true,
 
-new Ext.Application({
-    name: "NestedListDemo",
+		layout : 'fit',
+		enter : 'right',
+		exit : 'right',
 
-    launch: function() {
-	
+		// we always want the sheet to be 400px wide and to be as tall as the device
+		// allows
+		width : 400,
+		//height : 200,
+		stretchX : true,
+		stretchY : true,
 
-           
+		layout :
+		{
+			type : 'hbox',
+			align : 'stretch'
+		},
 
-var qquestions = [
-
-//var question1 = 
-insertQuestion(
-        "Question 1 : Dans quel domaine se font le plus d’accommodements raisonnables au Québec ?",
-        [
-            "Auprès des personnes handicapées", 
-            "Auprès des groupes religieux", 
-            "Auprès des immigrants"
-        ],
-        1,
-        "Les chiffres démontrent qu’au Québec, la majorité de la population considèrent qu’ils appartiennent à la religion catholique. Le catholicisme demeure un vecteur identitaire pour la plupart des Québécois. ",
-        1
-    ),
-    
-//var question2 = 
-insertQuestion(
-        "Question 2 : Selon les derniers chiffres de Statistique Canada, combien de Québécois s’identifient à la religion catholique ?",
-        ["498 788", "3 285 217", "5 939 720"],
-        1,
-        "Les chiffres démontrent qu’au Québec, la majorité de la population considèrent qu’ils appartiennent à la religion catholique. Le catholicisme demeure un vecteur identitaire pour la plupart des Québécois.<br>Les recherches récentes de Reginald Bibby (2011) l’amènent à conclure que la thèse de la sécularisation qui prétend que les êtres humains sont de moins en moins croyants et la thèse de la revitalisation qui observe une résurgence du phénomène religieux dans les sociétés modernes se font peu à peu remplacer  par la thèse de la polarisation. Bibby observe effectivement qu’un écart est de plus en plus grand entre croyants et athées, entre affiliation et non-affiliation à une religion et entre l’assistance hebdomadaire et la non-assistance au culte. Les individus sont de plus en plus croyants ou de plus en plus incroyants.",
-        2
-    ),    
-    
-//var question3 = 
-insertQuestion(
-        "Question 3 : Quel profil de personne retrouve-t-on principalement dans les organisations dites «sectes»?",
-        [
-            "Des gens vulnérables psychologiquement en recherche de réponses rapides", 
-            "Des personnes issues d’un milieu peu éduqué, démunies et naïves", 
-            "Des individus d’horizons divers représentant exactement la composition actuelle de la société qui sont en quête spirituelle"
-        ],
-        1,
-        "Les recherches ont démontré qu’il n’existe pas de profil d’individus décidant d’adhérer à une organisation sectaire pas plus qu’il existe un profil d’individus qui se convertit à une religion traditionnelle autre que celle de ses parents ou qui se dit athée. <br>Une secte se définit comme un groupe religieux ou idéologique créé en rupture contestataire envers une religion traditionnelle et/ou les valeurs des sociétés modernes. Un groupe devient sectaire en ce sens qu’il peut déraper vers une dangerosité, surtout si ses frontières avec la société sont durcies. La secte se situe cependant sur l’échelle des croyances extrêmes.n",
-        3
-    ),    
-
-//var question4 = 
-insertQuestion(
-        "Question 4 : Les hommes peuvent-ils être victimes de la polygamie?",
-        ["Oui", "Non"],
-        1,
-        "Les recherches ont démontré qu’il n’existe pas de profil d’individus décidant d’adhérer à une organisation sectaire pas plus qu’il existe un profil d’individus qui se convertit à une religion traditionnelle autre que celle de ses parents ou qui se dit athée. <br>Une secte se définit comme un groupe religieux ou idéologique créé en rupture contestataire envers une religion traditionnelle et/ou les valeurs des sociétés modernes. Un groupe devient sectaire en ce sens qu’il peut déraper vers une dangerosité, surtout si ses frontières avec la société sont durcies. La secte se situe cependant sur l’échelle des croyances extrêmes.n",
-        4
-    ), 
-//var question5 = 
-insertQuestion(
-        "Question 5 : En France, qui se pose comme un modèle de laïcité, est-ce que l’état est complètement séparé de l’Église?",
-        ["Oui", "Non"],
-        1,
-        "Les recherches ont démontré qu’il n’existe pas de profil d’individus décidant d’adhérer à une organisation sectaire pas plus qu’il existe un profil d’individus qui se convertit à une religion traditionnelle autre que celle de ses parents ou qui se dit athée. <br>Une secte se définit comme un groupe religieux ou idéologique créé en rupture contestataire envers une religion traditionnelle et/ou les valeurs des sociétés modernes. Un groupe devient sectaire en ce sens qu’il peut déraper vers une dangerosité, surtout si ses frontières avec la société sont durcies. La secte se situe cependant sur l’échelle des croyances extrêmes.n",
-        5
-    )//; 
-]    
-
-var resultat = new Ext.Panel({
-    floating: true,
-    centered: true,
-    modal: true,
-    width: '400px',
-    
-    dockedItems: [{
-        dock: 'top',
-        xtype: 'toolbar',
-        title: 'Résultat'
-    }
-    ],
-	items: [
-	
-        { html: "<h1>100 %</h1>recommencer." }
-            ]
-});
-
-function listQuestions()
+		dockedItems : [
 {
-    return questions;
+			dock : 'top',
+			xtype : 'toolbar',
+			title : 'Réponse'
+	},
+		/*{
+			xtype : 'toolbar',
+			dock : 'top',
+			items : [
+			{
+				text : 'Réponse'
+			}]
+		},*/
+
+		{
+			//fullscreen: true,
+
+			html : reponse,
+			/*layout :
+			{
+				type : 'hbox',
+				align : 'stretch'
+			},*/
+			//stretchY : true,
+			cls: 'texteExplication'
+		},
+
+		/*{
+		 //dock : 'top',
+		 xtype: 'panel',
+		 centered: true,
+		 html: 'contenu'
+		 },*/
+		/*{
+		 dock:
+		 html : 'Docked to the bottom'
+		 },*/
+		{
+			//dock : 'bottom',
+			xtype : 'button',
+			text : 'Question suivante',
+			style: 'margin-top: 10px;',
+			//width: '100px',
+			handler: function(e){
+				sheet.hide();
+				rootPanel.setActiveItem(nextActiveItem, 'cube');
+			}
+		}]
+	});
+
+
+	// Boutons des réponses
+	return new Ext.Button(
+	{
+		ui : 'confirm', //confirm
+		text : texte,
+		margin : 10,
+		//minWidth : "400px",
+		style : "min-width: 400px;",
+		handler : function()
+		{
+
+			//datePicker.show();
+			if (estBonneReponse) {
+				resultatFinal++;
+			}
+			sheet.show();
+
+			//var bravo = (succes) ? 'Bravo, vous avez raison!\n\n' : 'Ce n\'est pas tout à fait ça.';
+
+			/*
+			 var msg = new Ext.MessageBox(
+			 {
+			 html : 'text',
+			 width : '500px'
+			 });
+			 msg.alert('Réponse', "<p style=\"margin: 10px; font-size: 1.2em; font-weight:
+			 bold;\">" + bravo + "</p>" + reponse, function()
+			 {
+			 rootPanel.setActiveItem(nextActiveItem, 'cube');
+			 });
+			 */
+		}
+	});
 }
 
+// -----------------------------
 
-rootPanel = new Ext.Panel({
-    fullscreen: true,
-    layout: 'card',
-    items: questions //listQuestions()
-});
+// ----------------------------------
+// --- application
 
-            /*
-            Ext.regModel('modelReponses', {
-                fields: ['reponse']
-            });
-            
-            var store = new Ext.data.JsonStore({
-                model  : 'modelReponses',
-                //sorters: 'reponse',
-            
-                //getGroupString : function(record) {
-                //    return record.get('lastName')[0];
-                //},
-            
-                data: [
-                    {reponse: 'vrai'},
-                    {reponse: 'faux'}
-                ]
-            });
-            
-            var list = new Ext.List({
-                fullscreen: true,
-                
-                itemTpl : '{reponse}',
-                //grouped : true,
-                //indexBar: true,
-                
-                store: store
-            });
-            list.show();
-            */
-               
+new Ext.Application(
+{
+	name : "FaterTouch",
 
-	    
-/*
-        Ext.getBody().mask('Chargement...', 'x-mask-loading', false);
-        var panel = new Ext.Panel({
-            fullscreen: true,
-            layout: 'fit',
-            dockedItems: [{
-                xtype: 'toolbar',
-                dock: 'top',
-                items: [{
-                    text: 'First card',
-                    handler: function(){
-                        var carousel = panel.getComponent('carousel'); 
-                        carousel.setActiveItem(0, 'fade');
-                    }
-                },{
-                    text: 'Last Card',
-                    handler: function(){
-                        var carousel = panel.getComponent('carousel');
-                        carousel.setActiveItem(carousel.items.getCount() - 1, 'fade');
-                    }
-                }]
-            }]
-        });
-        
-        Ext.regModel('Card', {
-            fields: ['texte', 'cls']    
-        });
-        
-        var store = new Ext.data.Store({
-            model: 'Card',
-            proxy: {
-                type: 'ajax',
-                url: 'data.xml',
-                reader: {
-                    type: 'xml',
-                    record: 'card'
-                }
-            },
-            listeners: {
-                single: true,
-                datachanged: function(){
-                    Ext.getBody().unmask();
-                    var items = [];
-                    
-                    store.each(function(rec){
-                        items.push({
-                            html: rec.get('texte'),
-                            cls: 'card ' + rec.get('cls')
-                        });
-                    });
-                    
-                    var carousel = new Ext.Carousel({
-                        items: items,
-                        itemId: 'carousel',
-                        //draggable: false,
-                        scroll: false
-                    });
-                    carousel.setDraggable(false);
-                    panel.add(carousel);
-                    panel.doLayout();
-                }
-            }    
-        });
-        store.read();
-    	    */
-	    
-	
-    }
+	launch : function()
+	{
+		rootPanel = new Ext.Panel(
+		{
+			html : 'fond',
+			fullscreen : true,
+			layout : 'card', // comme un carrousel, mais manuel
+			items : questions
+		});
+
+	}
 });

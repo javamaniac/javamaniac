4ac5bd8222dfb6565835c822cb284fb5fc99c8a2
dominic.marcotte@gmail.com <dominic.marcotte@gmail.com@32b8cc01-7633-0410-beea-25b907642fa6>
2012-02-12 16:32:12 +0000

    Mise à jour des textes Apparence du bouton « Question suivante » Déplacemnent du fichier data.xml

--- FaterTouch/app2.js
+++ FaterTouch/app2.js
@@ -4,12 +4,35 @@
  * Veut souvent dire : ne trouve pas le fichier de données
  */
 
-var questions = [];
-var overlay;
-var rootPanel;
-var resultatFinal = 0;
-var nombreDeQuestion;
-var TITRE;
+
+// --- application
+
+new Ext.Application(
+{
+	name : "FaterTouch",
+	
+    icon: '/sencha-touch-1.1.1/exemples/kiva/resources/images/kiva.png',
+    glossOnIcon: false,
+    tabletStartupScreen: '/sencha-touch-1.1.1/exemples/kiva/resources/images/tablet_startup.png',
+
+	launch : function()
+	{
+		FaterTouch.rootPanel = new Ext.Panel(
+		{
+			html : '',
+			fullscreen : true,
+			layout : 'card', // comme un carrousel, mais manuel
+			items : FaterTouch.questions
+		});
+
+	}
+});
+
+
+FaterTouch.questions = [];
+FaterTouch.resultatFinal = 0;
+FaterTouch.nombreDeQuestion;
+FaterTouch.TITRE;
 
 
 
@@ -25,7 +48,7 @@ Ext.regModel('Titre',
 	proxy :
 	{
 		type : 'ajax',
-		url : 'data.xml',
+		url : 'data/data.xml',
 		reader :
 		{
 			type : 'xml',
@@ -33,16 +56,19 @@ Ext.regModel('Titre',
 		}
 	}
 });
-var storeTitre = new Ext.data.Store(
+FaterTouch.storeTitre = new Ext.data.Store(
 {
 	model : 'Titre',
 	autoLoad : true
 });
-storeTitre.load(
+FaterTouch.storeTitre.load(
 {
 	callback : function()
 	{
-		TITRE = storeTitre.first().get('titre');
+		console.debug('FaterTouch.storeTitre.load');
+		FaterTouch.TITRE = FaterTouch.storeTitre.first().get('titre');
+		//FaterTouch.resultatPanel.getDockedComponent('toolbar').setTitle('www');//FaterTouch.TITRE
+		resultatPanelToolbar.setTitle(FaterTouch.TITRE);
 	}
 });
 
@@ -58,7 +84,7 @@ Ext.regModel('Question',
 	proxy :
 	{
 		type : 'ajax',
-		url : 'data.xml',
+		url : 'data/data.xml',
 		reader :
 		{
 			type : 'xml',
@@ -73,7 +99,7 @@ Ext.regModel('Reponse',
 	proxy :
 	{
 		type : 'ajax',
-		url : 'data.xml',
+		url : 'data/data.xml',
 		reader :
 		{
 			type : 'xml',
@@ -82,20 +108,20 @@ Ext.regModel('Reponse',
 	}
 });
 
-var store = new Ext.data.Store(
+FaterTouch.store = new Ext.data.Store(
 {
 	model : 'Question',
 	autoLoad : true
 });
 
-store.load(
+FaterTouch.store.load(
 {
 	callback : function()
 	{
 
 		var itemCpt = 0;
 
-		store.each(function(question)
+		FaterTouch.store.each(function(question)
 		{
 			console.log("log Question : " + question.get('textQuestion') + ":");
 
@@ -106,19 +132,24 @@ store.load(
 				choixReponses.push(reponse.raw.textContent);
 			});
 			
-			questions.push(insertQuestion("<div class='titreNumQuestion'>Question " + (itemCpt + 1) + "</div>" + question.get('textQuestion'), choixReponses, question.get('bonneReponse'), question.get('texteExplicatif'), ++itemCpt));
+			FaterTouch.questions.push(insertQuestion("<div class='titreNumQuestion'>Question " + (itemCpt + 1) + "</div>" + question.get('textQuestion'), choixReponses, question.get('bonneReponse'), question.get('texteExplicatif'), ++itemCpt));
 
 		});
 		
-		nombreDeQuestion = itemCpt;
+		FaterTouch.nombreDeQuestion = itemCpt;
 		
-		questions.push([resultatPanel]);
+		FaterTouch.questions.push([FaterTouch.resultatPanel]);
 	}
 });
 
 
+var resultatPanelToolbar = new Ext.Toolbar({
+			dock : 'top',
+			xtype : 'toolbar',
+			title : FaterTouch.TITRE
+});
 
-var resultatPanel = new Ext.Panel(
+FaterTouch.resultatPanel = new Ext.Panel(
 	{
 		//floating : true,
 		centered : true,
@@ -134,11 +165,7 @@ var resultatPanel = new Ext.Panel(
 		},
 
 		dockedItems : [
-		{
-			dock : 'top',
-			xtype : 'toolbar',
-			title : TITRE
-		},
+			resultatPanelToolbar,
 		{
 			html : '' // initialisé plustard
 		},
@@ -150,7 +177,7 @@ var resultatPanel = new Ext.Panel(
 			//width: '100px',
 			handler: function(e){
 				//sheet.hide();
-				//rootPanel.setActiveItem(nextActiveItem, 'cube');
+				//FaterTouch.rootPanel.setActiveItem(nextActiveItem, 'cube');
 				window.location.reload();
 			}
 		}		
@@ -160,6 +187,7 @@ var resultatPanel = new Ext.Panel(
 		//html: '' // initialisé plustard
 
 	});
+
 
 
 // ----------------------------------
@@ -197,7 +225,7 @@ function insertQuestion(question, arrayReponses, bonneReponse, texteExplication,
 		{
 			dock : 'top',
 			xtype : 'toolbar',
-			title : TITRE
+			title : FaterTouch.TITRE
 		}],
 
 		items : items
@@ -233,7 +261,7 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 	 }
 	 });*/
 
-	var reponse = "<div class='bravoErreur'>" + ((estBonneReponse) ? 'Bravo, vous avez raison!' : 'Ce n\'est pas tout à fait ça.') + "</div>" + texteExplication;
+	var reponse = "<div class='bravoErreur'>" + ((estBonneReponse) ? 'Bonne réponse!' : 'Mauvaise réponse!') + "</div>" + texteExplication;
 
 
 	// volet de la bonne réponse
@@ -241,6 +269,7 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 	{
 		modal : true,
 		centered : false,
+
 		//hideOnMaskTap : true,
 
 		layout : 'fit',
@@ -249,7 +278,7 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 
 		// we always want the sheet to be 400px wide and to be as tall as the device
 		// allows
-		width : 400,
+		width : 500,
 		//height : 200,
 		stretchX : true,
 		stretchY : true,
@@ -264,6 +293,7 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 {
 			dock : 'top',
 			xtype : 'toolbar',
+			cls: 'toolbarReponse',			
 			title : 'Réponse'
 	},
 		/*{
@@ -277,7 +307,7 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 
 		{
 			//fullscreen: true,
-
+			scroll: 'vertical',
 			html : reponse,
 			/*layout :
 			{
@@ -299,15 +329,24 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 		 html : 'Docked to the bottom'
 		 },*/
 		{
-			//dock : 'bottom',
-			xtype : 'button',
-			text : 'Question suivante',
-			style: 'margin-top: 10px;',
-			//width: '100px',
-			handler: function(e){
-				sheet.hide();
-				rootPanel.setActiveItem(nextActiveItem, 'flip'); //cube, slide 
-			}
+			dock : 'bottom',
+			xtype : 'toolbar',
+			cls: 'toolbarReponse',
+			items: [
+				{
+					xtype : 'button',
+					text : 'Question suivante ⇨',
+					//style: 'margin-top: 10px;',
+					cls: 'boutonQuestionSuivante',
+					centered : true,
+					//width: '100px',
+					handler: function(e){
+						sheet.hide();
+						FaterTouch.rootPanel.setActiveItem(nextActiveItem, 'flip'); //cube, slide 
+					}
+					
+				}
+			]
 		}]
 	});
 
@@ -327,43 +366,61 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 
 			//datePicker.show();
 			if (estBonneReponse) {
-				resultatFinal++;
-				console.log("Bonne réponse : " + resultatFinal);
+				FaterTouch.resultatFinal++;
+				//console.log("Bonne réponse : " + FaterTouch.resultatFinal);
 			}
+			/*
 			else
 			{
-				console.log("Mauvaise réponse : " + resultatFinal);
-			}
+				console.log("Mauvaise réponse : " + FaterTouch.resultatFinal);
+			}*/
 
-			var xBonneReponse = (resultatFinal > 1) ? " bonnes réponses" : " bonne réponse";
+			var xBonneReponse = (FaterTouch.resultatFinal > 1) ? " bonnes réponses" : " bonne réponse";
+			var resultatIntroduction = (FaterTouch.resultatFinal < 4) ? "Besoin d’augmenter votre score?" : "Vous avez une bonne connaissance du religieux contemporain. Vous seriez intéressés à approfondir certaines de ces questions?";
 			var texteFinal = '' +
 				'<div>' +
 				'	<div>&nbsp;</div>' +
 				'	<div class="blocInfo blocResultat">' +
 				'		<div>' +
-				'			<img src="images/etoile.jpg" style="' + ((resultatFinal>0) ? '' : 'opacity: 0.3;') + '"/>' +
-				'			<img src="images/etoile.jpg" style="' + ((resultatFinal>1) ? '' : 'opacity: 0.3;') + '"/>' +
-				'			<img src="images/etoile.jpg" style="' + ((resultatFinal>2) ? '' : 'opacity: 0.3;') + '"/>' +
-				'			<img src="images/etoile.jpg" style="' + ((resultatFinal>3) ? '' : 'opacity: 0.3;') + '"/>' +
-				'			<img src="images/etoile.jpg" style="' + ((resultatFinal>4) ? '' : 'opacity: 0.3;') + '"/>' +
+				'			<img src="images/etoile.jpg" style="' + ((FaterTouch.resultatFinal>0) ? '' : 'opacity: 0.3;') + '"/>' +
+				'			<img src="images/etoile.jpg" style="' + ((FaterTouch.resultatFinal>1) ? '' : 'opacity: 0.3;') + '"/>' +
+				'			<img src="images/etoile.jpg" style="' + ((FaterTouch.resultatFinal>2) ? '' : 'opacity: 0.3;') + '"/>' +
+				'			<img src="images/etoile.jpg" style="' + ((FaterTouch.resultatFinal>3) ? '' : 'opacity: 0.3;') + '"/>' +
+				'			<img src="images/etoile.jpg" style="' + ((FaterTouch.resultatFinal>4) ? '' : 'opacity: 0.3;') + '"/>' +
 				'		</div>' +
-				'		<div>Vous avez obtenu<br/>' + resultatFinal + xBonneReponse + ' sur 5.</div>' +
-				'		<div class="texte" style="font-size: 0.8em; width:75%; margin:auto;font-weight: normal;margin-top: 20px;text-align: left;">' +
-				'			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.' +
+				'		<div>Vous avez obtenu<br/>' + FaterTouch.resultatFinal + xBonneReponse + ' sur 5.</div>' +
+				'		<div class="texteResultat" style="font-size: 0.8em; margin:auto;font-weight: normal;margin-top: 20px;text-align: left;">' +
+				'			<div>' + resultatIntroduction + '</div>' + 
+				'			<div>Une formation en étude du religieux contemporain vous exposerait aux thématiques abordées par ce questionnaire. Voici quelques titres de cours donnés à la Faculté&nbsp;:</div>' +
+				'			<div><ul>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/shr332">Contacts entre cultures et religions</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/erc712">Éthiques religieuses et débats sociaux</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/thl760">Les nouveaux mouvements religieux</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/erc300">Femmes, religions et spiritualités</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/erc301">Faits religieux et médias</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/erc302">Égalité femme/homme, laïcité et religions</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/thp704">Santé, souffrance et soins spirituels</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/erc304">Sexualité, amour et spiritualités</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/shr886">La formation des identités religieuses</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/shr114">Phénoménologie de la religion au Québec</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/shr895">Le religieux contemporain au Canada</a></li>' +
+				'				<li><a target="_blank" href="http://www.usherbrooke.ca/fiches-cours/shr896">Religion, politique et identité</a></li>' +
+				'			</ul></div>' +
+				'			<div>Sans oublier <a target="_blank" href="http://www.USherbrooke.ca/fater/ecole-ete">l’École d’été</a> sur les religions en modernité avancée qui se donne au Campus de Longueuil.</div>' + 
 				'		</div>' +
 				'	</div>' +
 				'</div>';
 			
 			// TODO à ne faire qu'après la dernière question
-			resultatPanel.update(
+			FaterTouch.resultatPanel.update(
 			{
 				html : texteFinal 
-				//'<div>&nbsp;</div><div class="blocInfo blocResultat">Vous avez obtenu<br/>' + resultatFinal + xBonneReponse + ' sur 5. </div><div class="texte" style="font-size:1.0em; width:75%; margin:auto;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br/>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br/>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>' //<span class="resultatFinal">' + (resultatFinal * 100 / nombreDeQuestion) + ' %</span></div>'
+				//'<div>&nbsp;</div><div class="blocInfo blocResultat">Vous avez obtenu<br/>' + FaterTouch.resultatFinal + xBonneReponse + ' sur 5. </div><div class="texte" style="font-size:1.0em; width:75%; margin:auto;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br/>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br/>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>' //<span class="FaterTouch.resultatFinal">' + (FaterTouch.resultatFinal * 100 / FaterTouch.nombreDeQuestion) + ' %</span></div>'
 			});		
 
 				
 				
-				//'<div class="blocInfo blocResultat">Votre résultat : <span class="resultatFinal">' + (resultatFinal * 100 / nombreDeQuestion) + ' %</span></div>');
+				//'<div class="blocInfo blocResultat">Votre résultat : <span class="FaterTouch.resultatFinal">' + (FaterTouch.resultatFinal * 100 / FaterTouch.nombreDeQuestion) + ' %</span></div>');
 			
 			sheet.show();
 
@@ -378,7 +435,7 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 			 msg.alert('Réponse', "<p style=\"margin: 10px; font-size: 1.2em; font-weight:
 			 bold;\">" + bravo + "</p>" + reponse, function()
 			 {
-			 rootPanel.setActiveItem(nextActiveItem, 'cube');
+			 FaterTouch.rootPanel.setActiveItem(nextActiveItem, 'cube');
 			 });
 			 */
 		}
@@ -388,21 +445,3 @@ function creationBouton(texte, estBonneReponse, texteExplication, nextActiveItem
 // -----------------------------
 
 // ----------------------------------
-// --- application
-
-new Ext.Application(
-{
-	name : "FaterTouch",
-
-	launch : function()
-	{
-		rootPanel = new Ext.Panel(
-		{
-			html : 'fond',
-			fullscreen : true,
-			layout : 'card', // comme un carrousel, mais manuel
-			items : questions
-		});
-
-	}
-});
